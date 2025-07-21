0e47b19cddc4cfe8099e7c53830eaf5770dc7d55
wky95 <wangkayu0512@gmail.com>
2025-07-22 00:02:19 +0800

    MPoly.hpp

--- MPoly.hpp
+++ MPoly.hpp
@@ -15,31 +15,23 @@ struct MPoly {
     MPoly(const MPoly &b, const int x) { a = b.a; resize(x); }
     MPoly(const vector<mint> b) { a = b; }
     MPoly(const vector<mint> &b, const int x) { a = b; resize(x); }
-    inline void extend(const MPoly &b){
-		a.insert(a.end(), b.a.begin(), b.a.end());
-	}
+    inline void extend(const MPoly &b){ a.insert(a.end(), b.a.begin(), b.a.end()); }
+    MPoly& operator+=(const int x) { a[0] += x; return *this; }
+    MPoly& operator-=(const int x) { a[0] -= x; return *this; }
     MPoly& operator+=(const MPoly &b){
+        if (ssize(a) < ssize(b.a)) resize(ssize(b.a));
+        for (int i = 0; i < ssize(b.a); i++) a[i] += b.a[i];
+        return *this;
+    }
+    MPoly& operator-=(const MPoly &b){
 		if (ssize(a) < ssize(b.a)) resize(ssize(b.a));
-		for (int i = 0; i < ssize(b.a); i++) a[i] += b.a[i];
-		return *this;
-	}
-    MPoly& operator+=(const int x) {
-        a[0] += x;
+		for (int i = 0; i < ssize(b.a); i++) a[i] -= b.a[i];
 		return *this;
 	}
     MPoly operator - () const {
 		MPoly res(*this);
 		for (auto &i : res.a) i = -i;
 		return res;
-	}
-    MPoly& operator-=(const MPoly &b){
-		if (ssize(a) < ssize(b.a)) resize(ssize(b.a));
-		for (int i = 0; i < ssize(b.a); i++) a[i] -= b.a[i];
-		return *this;
-	}
-    MPoly& operator-=(const int x) {
-        a[0] -= x;
-		return *this;
 	}
     MPoly& operator*=(const int x){
 		for (auto &i : a) i *= x;
@@ -65,34 +57,9 @@ struct MPoly {
 #define make_mpoly_operator(op) inline MPoly operator op (const MPoly &b) const { MPoly res(*this); return (res op##=b); }
 #define make_int_operator(op)   inline MPoly operator op (const int x)    const { MPoly res(*this); return (res op##=x); }
 	make_mpoly_operator(+) make_mpoly_operator(-) make_mpoly_operator(*)
-	make_int_operator(+) make_int_operator(-) make_int_operator(*) make_int_operator(/)
+	make_int_operator(+)   make_int_operator(-)   make_int_operator(*)   make_int_operator(/)
 #undef make_mpoly_operator
 #undef make_int_operator
-    MPoly inv() {
-        int sz = ssize(a);
-        if (sz == 1) return MPoly({a[0].inverse()});
-        int len = 2 << __lg(sz - 1);
-        MPoly g(1); g.a[0] = a[0].inverse();
-        for (int l = 1; l < len; l <<= 1) {
-            (g *= 2) -= MPoly(a, l << 1).mulFix(g * g);
-        }
-        g.resize(sz);
-        return g;
-    }
-    pair<MPoly, MPoly> divide(MPoly b) {
-        // a = bQ + R, O(NlogN), b.back() != 0
-        int n = ssize(a), m = ssize(b.a), k = n - m + 1;
-        MPoly Q, R(*this);
-        if (n >= m) {
-            MPoly ra(vector<mint>(rbegin(a), rend(a)), k); 
-            MPoly rb(vector<mint>(rbegin(b.a), rend(b.a)), k);
-            Q = ra.mulFixconst(rb.inv()); reverse(all(Q.a));
-            R -= b * Q;
-        }
-        while (ssize(Q.a) and Q.a.back() == 0) Q.a.pop_back();
-        while (ssize(R.a) and R.a.back() == 0) R.a.pop_back();
-        return {Q, R};
-    }
     MPoly diff() {
 		MPoly res(ssize(a) - 1);
         for (int i = 1; i < ssize(a); i++) res.a[i-1] = a[i] * i;
@@ -103,17 +70,27 @@ struct MPoly {
 		for (int i = 1; i <= ssize(a); i++) res.a[i] = a[i-1] / i;
 		return res;
 	}
-    MPoly log() { return (this->inv().mulFix(this->diff())).integ(); }
-    MPoly exp() {
-        int len = (ssize(a) == 1 ? 1 : 2 << __lg(ssize(a) - 1));
-        MPoly g(1); g[0] = 1;
-        for (int l = 1; l < len; l <<= 1) {
-            g.resize(l << 1);
-            g = ((MPoly(a, l << 1) + 1) - g.log()).mulFixconst(g);
+    MPoly inv() {
+        MPoly g(1); g[0] = a[0].inverse();
+        if (ssize(a) > 1) {
+            for (int l = 1, len = 2 << __lg(ssize(a) - 1); l < len; l <<= 1) 
+                (g *= 2) -= MPoly(a, l << 1).mulFix(g * g);
+            g.resize(ssize(a));
         }
-        g.resize(ssize(a));
         return g;
     }
+    MPoly exp() {
+        MPoly g(1); g[0] = 1;
+        if (ssize(a) > 1) {
+            for (int l = 1, len = 2 << __lg(ssize(a) - 1); l < len; l <<= 1) {
+                g.resize(l << 1);
+                g = ((MPoly(a, l << 1) + 1) - g.log()).mulFixconst(g);
+            }
+            g.resize(ssize(a));
+        }
+        return g;
+    }
+    MPoly log() { return (this->inv().mulFix(this->diff())).integ(); }
     MPoly pow(int64_t n) {
         int i = find_if_not(begin(a), end(a), [](auto x){ return x == mint(0); }) - begin(a);
         if (i and (n >= ssize(a) or n * i >= ssize(a))) return MPoly<mod>(ssize(a));
@@ -125,6 +102,20 @@ struct MPoly {
         for (int j = 0; j < min<int32_t>(ssize(res1.a), (ssize(a) - n * i)); j++) 
             res2.a[j + n * i] = res1.a[j];
         return res2;
+    }
+    pair<MPoly, MPoly> divide(MPoly b) {
+        // a = bQ + R, O(NlogN), b.back() != 0
+        int n = ssize(a), m = ssize(b.a), k = n - m + 1;
+        MPoly Q, R(*this);
+        if (n >= m) {
+            MPoly ra(vector<mint>(rbegin(a), rend(a)), k); 
+            MPoly rb(vector<mint>(rbegin(b.a), rend(b.a)), k);
+            Q = ra.mulFixconst(rb.inv()); reverse(begin(Q.a), end(Q.a));
+            R -= b * Q;
+        }
+        while (ssize(Q.a) and Q.a.back() == 0) Q.a.pop_back();
+        while (ssize(R.a) and R.a.back() == 0) R.a.pop_back();
+        return {Q, R};
     }
     MPoly evaluate(MPoly x) { // calculate for each i y_i = a(x_i)
         if (!ssize(x.a)) return {};
